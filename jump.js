dd7a5c00ddab1b027bd00f8eebb0b0bef50bd4f6
Rythiman <rythiman@hotmail.com>
2014-04-08 10:33:34 -0500

    Reduced Code: index.html, jump.css, jump.js
    Deleting nearly 100 lines of unnecessary space, it is much easier to
    scroll through and read these files.

--- jump.js
+++ jump.js
@@ -1,94 +1,61 @@
 /**
  * @author HMG
  */
+//THE MAIN GAME ENGINE 
 
-/**
- *THE MAIN GAME ENGINE 
- */
-
-//Get canvas element and shape canvas.
-var canvas = document.getElementById("canvas");
-canvas.width = 1000;
-canvas.height = 500;
+var canvas = document.getElementById("canvas"); canvas.width = 1000; canvas.height = 500; //Get canvas element and shape canvas.
 var ctx = canvas.getContext("2d");
 
-var defaultFont = "12px Arial";			//Set Default Font
+var defaultFont = "12px Arial";	ctx.font = defaultFont; 		//Set Default Font
 
-ctx.font = defaultFont; 
-
-/**OBJECT GAME: Specifies game mode*/
-var game = 
-{
+var game = { 		//OBJECT GAME: Specifies game mode
 	mode:0,			//Game Mode (0: Start Menu, 1: Game, 2: Pause Menu)
 	FPS:60,			//FPS
 };
 
-//Prepare game audio
-var jump = document.getElementById("jump");
+var jump = document.getElementById("jump"); //Prepare game audio
 
-//Draw Function of all objects grouped together
-function draw()
-{
-	player.draw();
-	floor.draw();
+function draw(){		//Draw Function of all objects grouped together
+	player.draw(); floor.draw();
 }
 
-//Physics function of all objects grouped together
-function physics()
-{
-	player.physics();
-	floor.physics();
+function physics(){ 	//Physics function of all objects grouped together
+	player.physics(); floor.physics();
 }
 
-//Function: Clear the entire screen
-function clearscreen()
-{
+function clearscreen(){			//Function: Clear the entire screen
 	ctx.clearRect (0,0,1000,500);
 }
 
-//Function: Display pause menu
-function pausemenu()
-{
-	clearscreen();
-	ctx.fillText ("Paused: Press enter to resume play", 450,250);
+function pausemenu(){			//Function: Display pause menu
+	clearscreen(); ctx.fillText ("Paused: Press enter to resume play", 450,250);
 }
 
-//Tick: What occurs during each game tick
-function tick()
-{
-	if (game.mode ===0)			//Start Menu
-	{
-		clearscreen();
-		ctx.fillText ("Press enter to begin", 450,250);
-	}
-	if (game.mode == 1)			//Game
-	{
-		clearscreen();
-		physics();
-		draw();
-
+function tick(){			//Tick: What occurs during each game tick
+	if (game.mode ===0){	//Start Menu
+		clearscreen(); ctx.fillText ("Press enter to begin", 450,250);
 	}
 	
+	if (game.mode == 1){		//Game
+		clearscreen(); physics(); draw();
+	}
 	
-	if (game.mode == 2)			//Pause
-	{
+	if (game.mode == 2){		//Pause
 		pausemenu();
 	}
-	ctx.fillText ("Game Mode: " + game.mode, 10, 30);
+	
+	ctx.fillText ("Game Mode: " + game.mode, 10, 30); //Displays game mode for debugging purposes.
 }
 
+var animFrame = window.requestAnimationFrame || 	//sets up animation frame, an alternative to setInterval
+	window.webkitRequestAnimationFrame || 			//browsers never seem to agree
+	window.mozRequestAnimationFrame || 
+	window.oRequestAnimationFrame || 
+	window.msRequestAnimationFrame ||
+    null ;
 
-    var animFrame = window.requestAnimationFrame ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame    ||
-            window.oRequestAnimationFrame      ||
-            window.msRequestAnimationFrame     ||
-            null ;
-
-    var recursiveAnim = function() {
-        tick();
-        animFrame( recursiveAnim );
+    var recursiveAnim = function() { 		//set up animation loop
+        tick(); animFrame( recursiveAnim );
     };
 
-    // start the game tick
-    animFrame( recursiveAnim );
+animFrame( recursiveAnim ); // start the game tick
